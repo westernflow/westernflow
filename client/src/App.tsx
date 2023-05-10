5a570feb876b5e98ffc968007af862357a7574f0
mnnaegel <maxnaegel@gmail.com>
2023-05-10 00:10:22 -0400

    Add homepage and search bar for courses

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,49 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Course } from './Models/definitions';
-import { useEffect } from 'react';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import HomePresenter from "./components/HomePresenter";
 
 function App() {
-  // fetch data from backend API endpoint http://localhost:8080/api/v1/courses?course-faculty=exact:PSYCHOL&section-class-number=exact:8045 
-  // turn it into json data then display it in the browser
-  
-  // useState to store json data
-  const [courses, setCourses] = React.useState<Course[]>([]);
-
-  // useEffect to fetch data from backend API endpoint
-  useEffect(() => {
-    fetch('http://localhost:8080/api/v1/courses?course-faculty=exact:PSYCHOL')
-      .then(response => response.json())
-      .then(data => setCourses(data as Course[]))
-      .then(() => console.log("RERENDER"))
-  }, []);
-
-  useEffect(() => {
-    console.log(courses);
-  }, [courses]);
-   
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {courses.map((course,idx) => (
-              <React.Fragment key={idx}><br/>PSYCHOL {course.courseData.Number}: {course.courseData.Name}</React.Fragment>
-          ))
-          }              
-        </ul>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePresenter} />
+        <Route path="/search/:search" element={<div>search result hi</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/SearchBar.tsx
@@ -0,0 +1,74 @@
+import { data } from "autoprefixer";
+import React, { useState, useRef, useEffect, useCallback } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
+function SearchBar (){
+  const history = useNavigate();
+
+  const searchRef = useRef<HTMLInputElement>(null);
+
+  const [input, setInput] = useState<string>("");
+  const [showResults, setShowResults] = useState(false);
+  const [searchResults, setSearchResults] = useState<string[]>([]);
+
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (searchRef.current && !searchRef.current.contains(event.target)) { // check if user clicked on outside (not contained in event.target)
+        setTimeout(()=>setShowResults(false),100);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [searchRef]);
+
+  const goToSearchResult = (result : string) => {
+    // remove spaceas from result
+    result = result.replace(/\s/g, '');
+    history(`/course/${result}`);
+  }
+
+  const transformData = (data: string[]): string[] => {
+    // first filter for the first 5 elements
+    data = data.filter((_, i) => i < 8);
+
+    // split the left and right side of the data into course name and course number by looking for the first numeric character
+    data = data.map((result) => {
+      const index = result.search(/\d/);
+      return result.slice(0, index) + " " + result.slice(index);
+    })
+    
+    return data
+  }
+
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+    if (e.target.value.length > 0) {
+      setShowResults(true);
+      fetch(`http://localhost:8080/api/v1/search/${e.target.value}`)
+        .then((res) => res.json())
+        .then((data: string[]) => setSearchResults(transformData(data)));
+    } else {
+      setShowResults(false);
+    }
+  }
+
+  return (
+    <div className="flex flex-col border-2 border-gray-300">
+          <input
+            ref={searchRef}
+            onFocus={() => setShowResults(true)}
+            className="truncate flex-grow py-3 px-4 text-gray-700 leading-tight text-lg focus:outline-1 focus:border-blue-500"
+            id="search"
+            type="text"
+            placeholder="Search for a professor or course..."
+            onChange={(e) => handleInputChange(e)}
+          />
+          {showResults ? searchResults.map((result, i) => (<div key={i} onClick={() => goToSearchResult(result)} className="font-bold border-t-2 border-b-1 border-gray-300 bg-white flex-grow py-2 px-4 text-gray-700 leading-tight text-lg focus:outline-1 hover:bg-blue-500 hover:text-white cursor-pointer">{result}</div>)) : null}
+        </div>
+  )
+}
+
+export default SearchBar
