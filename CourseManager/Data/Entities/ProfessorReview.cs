41ed0b1fff43bf23327e0b577c00bc00e7d665ae
mnaegel <mnaegel@uwaterloo.ca>
2024-01-24 15:40:32 -0500

    Professor ReviewText limit migration

--- CourseManager/Data/Entities/ProfessorReview.cs
+++ CourseManager/Data/Entities/ProfessorReview.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Data.Interfaces;
 
 namespace Data.Entities;
@@ -15,6 +16,7 @@ public class ProfessorReview : IEntity
     public int Quality { get; set; }
     public int Difficulty { get; set; }
     public DateTime Date { get; set; } 
+    [StringLength(500)]
     public string? ReviewText { get; set; }
     public int Helpful { get; set; }
     public int Clarity { get; set; }
