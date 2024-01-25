a6d62e7eec8e27b2a46ec74fa43b6842feddee03
mnaegel <mnaegel@uwaterloo.ca>
2024-01-25 12:37:13 -0500

    Scraper worker template init
    rename

---
+++ CourseManager/Workers/Program.cs
@@ -0,0 +1,13 @@
+namespace Scrapers;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+            .Build();
+
+        host.Run();
+    }
+}
