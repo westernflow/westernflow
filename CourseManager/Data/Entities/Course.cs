72f30e05b23a3d2ec9a35b45a658f086b1880605
mnaegel <mnaegel@uwaterloo.ca>
2024-01-29 17:31:24 -0500

    Make internal courseId to string
    Avoid case where id has leading zero "011..."

--- CourseManager/Data/Entities/Course.cs
+++ CourseManager/Data/Entities/Course.cs
@@ -7,11 +7,43 @@ using Data.Interfaces;
 
 namespace Data.Entities;
 
+public record CourseConstructorParams()
+{
+    public int Number { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public string? PrerequisiteString { get; init; }
+    public string? CorequisiteString { get; init; }
+    public string? AntirequisiteString { get; init; }
+    public string? Description { get; init; }
+    public string? ExtraInformation { get; init; }
+    public decimal? Weight { get; init; }
+    public BreadthCategory BreadthCategory { get; init; }
+    public string InternalCourseId { get; init; } = string.Empty;
+    public int? SourceInfoId { get; init; }
+    public int FacultyId { get; init; }
+}
+
 public class Course : IEntity
 {
     // see https://stackoverflow.com/questions/54400115/no-suitable-constructor-found-for-entity-type-string
     private Course()
     {
+    }
+    
+    public Course([NotNull] CourseConstructorParams courseConstructorParams)
+    {
+        Number = courseConstructorParams.Number;
+        Name = courseConstructorParams.Name;
+        PrerequisiteString = courseConstructorParams.PrerequisiteString;
+        CorequisiteString = courseConstructorParams.CorequisiteString;
+        AntirequisiteString = courseConstructorParams.AntirequisiteString;
+        Description = courseConstructorParams.Description;
+        ExtraInformation = courseConstructorParams.ExtraInformation;
+        Weight = courseConstructorParams.Weight;
+        BreadthCategory = courseConstructorParams.BreadthCategory;
+        InternalCourseId = courseConstructorParams.InternalCourseId;
+        SourceInfoId = courseConstructorParams.SourceInfoId;
+        FacultyId = courseConstructorParams.FacultyId;
     }
 
     [Key]
@@ -34,7 +66,8 @@ public class Course : IEntity
     [Column(TypeName = "decimal(2,2)")]
     public decimal? Weight { get; set; } 
     public BreadthCategory BreadthCategory { get; set; }
-    public int InternalCourseId { get; set; }
+    [StringLength(20)]
+    public string InternalCourseId { get; set; } = string.Empty;
     
     public int? SourceInfoId { get; set; }
     [ForeignKey(nameof(SourceInfoId))]
@@ -49,10 +82,4 @@ public class Course : IEntity
     
     [InverseProperty(nameof(ProfessorReview.Course))]
     public virtual ICollection<ProfessorReview> RelatedProfessorReviews { get; set; } = null!;
-    
-    public Course(Faculty faculty, string name)
-    {
-        Faculty = faculty;
-        Name = name;
-    }
 }
