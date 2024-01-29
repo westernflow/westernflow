8a53a657ac737cfa08f39ac7b1017222127c4120
mnaegel <mnaegel@uwaterloo.ca>
2024-01-29 17:24:40 -0500

    Add unique constraint to course offerings

---
+++ CourseManager/Data/Migrations/20240129222414_UniqueConstraintToCourseOffering.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class UniqueConstraintToCourseOffering : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseOffering_Year_Suffix_CourseId",
+                table: "CourseOffering",
+                columns: new[] { "Year", "Suffix", "CourseId" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CourseOffering_Year_Suffix_CourseId",
+                table: "CourseOffering");
+        }
+    }
+}
