fdbc8461bd1612d0fa22c6c4a210521a0fa91ec2
mnaegel <mnaegel@uwaterloo.ca>
2024-01-30 13:06:01 -0500

    Database reset bash script

---
+++ CourseManager/Workers/clear_db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Database connection details
+DB_HOST="localhost"
+DB_PORT="5432"
+DB_USER="postgres"
+DB_PASS="example"
+DB_NAME="postgres"
+
+# SQL command to delete rows from tables
+SQL_COMMAND="
+DELETE FROM \"TimingDetails\";
+DELETE FROM \"Sections\";
+DELETE FROM \"CourseOffering\";
+DELETE FROM \"Courses\";"
+
+# Execute SQL command
+PGPASSWORD=$DB_PASS psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -c "$SQL_COMMAND"
+
