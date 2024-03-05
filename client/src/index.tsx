7c8920339df561100d9e27d9a587606efe3e24ef
mnaegel <mnaegel@uwaterloo.ca>
2024-03-05 10:59:41 -0500

    Add Chakra

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,20 @@
-import { RelayEnvironmentProvider } from "react-relay";
-import { RelayEnvironment } from "./RelayEnvironment";
+import {RelayEnvironmentProvider} from "react-relay";
+import {RelayEnvironment} from "./RelayEnvironment";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import {ChakraProvider} from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 root.render(
-  <RelayEnvironmentProvider environment={RelayEnvironment}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </RelayEnvironmentProvider>
+	<RelayEnvironmentProvider environment={RelayEnvironment}>
+		<React.StrictMode>
+			<ChakraProvider>
+				<App/>
+			</ChakraProvider>
+		</React.StrictMode>
+	</RelayEnvironmentProvider>
 );
