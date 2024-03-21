e9de4dacd84c9c4a9dc5e24a5a4e00e2639e093a
Felo Tadros <felotadros326@gmail.com>
2024-03-21 19:01:15 -0400

    Add localstorage caching (#25)
    Co-authored-by: mnaegel <mnaegel@uwaterloo.ca>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,50 +1,53 @@
-import {Route, Routes, BrowserRouter} from "react-router-dom";
-import {RelayEnvironmentProvider} from "react-relay";
-import {createRelayEnvironment} from "./RelayEnvironment";
-import Home from "./presenters/HomePresenter";
-import {CoursePresenter} from "./presenters/CoursePresenter";
-import {Suspense, useEffect, useState} from "react";
-import {useAuth0} from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Suspense, useEffect, useState } from "react";
+import { RelayEnvironmentProvider } from "react-relay";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RelayModernEnvironment from "relay-runtime/lib/store/RelayModernEnvironment";
+import { createRelayEnvironment } from "./RelayEnvironment";
+import { CoursePresenter } from "./presenters/CoursePresenter";
+import Home from "./presenters/HomePresenter";
 
 const SuspendedCoursePresenter = () => {
-	return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<CoursePresenter/>
-		</Suspense>
-	)
-}
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <CoursePresenter />
+        </Suspense>
+    );
+};
 
 function App() {
-	const { getAccessTokenSilently } = useAuth0();
-	const [relayEnvironment, setRelayEnvironment] = useState<RelayModernEnvironment>();
+    const { getAccessTokenSilently } = useAuth0();
+    const [relayEnvironment, setRelayEnvironment] =
+        useState<RelayModernEnvironment>();
 
-	useEffect(() => {
-		console.log("Relay environment has changed..")
-		const tokenProvider = {
-			getToken: () => getAccessTokenSilently().catch(() => ""),
-		};
+    useEffect(() => {
+        console.log("Relay environment has changed..");
+        const tokenProvider = {
+            getToken: () => getAccessTokenSilently().catch(() => ""),
+        };
 
-		setRelayEnvironment(createRelayEnvironment(tokenProvider));
-	}, [getAccessTokenSilently]);
-	
-	if (!relayEnvironment) {
-		return <div>Loading Relay environment...</div>;
-	}
+        setRelayEnvironment(createRelayEnvironment(tokenProvider));
+    }, [getAccessTokenSilently]);
 
-	return (
-		<RelayEnvironmentProvider environment={relayEnvironment}>
-			<div className="roboto">
-				<BrowserRouter>
-					<Routes>
-						<Route path="/" Component={Home}/>
-						<Route path="/course/:courseCode"
-						       Component={SuspendedCoursePresenter}/>
-					</Routes>
-				</BrowserRouter>
-			</div>
-		</RelayEnvironmentProvider>
-	)
+    if (!relayEnvironment) {
+        return <div>Loading Relay environment...</div>;
+    }
+
+    return (
+        <RelayEnvironmentProvider environment={relayEnvironment}>
+            <div className="roboto">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" Component={Home} />
+                        <Route
+                            path="/course/:courseCode"
+                            Component={SuspendedCoursePresenter}
+                        />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </RelayEnvironmentProvider>
+    );
 }
 
 export default App;
--- client/src/RelayEnvironment.ts
+++ client/src/RelayEnvironment.ts
@@ -6,28 +6,72 @@ import {
 } from "relay-runtime";
 import fetchWrapper from "./api/fetchWrapper";
 
+const CACHE_KEY_PREFIX = 'relayCache_';
+
+const generateCacheKey = (query: any, variables:any) => {
+	const queryID = btoa(query.text);
+	const variablesID = btoa(JSON.stringify(variables));
+	return `${CACHE_KEY_PREFIX}${queryID}_${variablesID}`;
+};
+
+const readFromCache = (key:any) => {
+	const { data, timestamp } = JSON.parse(localStorage.getItem(key) || '{}');
+	const now = new Date().getTime();
+	const oneHour = 60 * 60 * 1000;
+	
+	if (timestamp && now - timestamp < oneHour) {
+		return JSON.parse(data);
+	}
+};
+
+const writeToCache = (key:any, data:any) => {
+	const item = {
+		data: JSON.stringify(data),
+		timestamp: new Date().getTime(),
+	};
+	localStorage.setItem(key, JSON.stringify(item));
+};
+
+
 interface TokenProvider {
     getToken: () => Promise<string>;
 }
 
 const createFetchFn = (tokenProvider: TokenProvider) => {
-    return async (request: any, variables: any) => {
-        const resp = await fetchWrapper('graphql', {
-        method: "POST",
-        mode: "cors",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${await tokenProvider.getToken()}`,
-        },
-        body: JSON.stringify({
-            query: request.text, // <-- The GraphQL document composed by Relay
-            variables,
-        }),
-        });
-    
-        return await resp.json();
-    };
-}
+	return async (request:any, variables:any) => {
+		if (request.name === "SearchBarIndexedCoursesQuery") {
+			const cacheKey = generateCacheKey(request, variables);
+			const cachedData = readFromCache(cacheKey);
+
+			if (cachedData) {
+				return cachedData;
+			}
+		}
+
+		const resp = await fetchWrapper('graphql', {
+			method: "POST",
+			mode: "cors",
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": `Bearer ${await tokenProvider.getToken()}`,
+			},
+			body: JSON.stringify({
+				query: request.text,
+				variables,
+			}),
+		});
+
+		const jsonData = await resp.json();
+		
+		if (request.name === "SearchBarIndexedCoursesQuery") {
+			const cacheKey = generateCacheKey(request, variables);
+			writeToCache(cacheKey, jsonData);
+		}
+
+		return jsonData;
+	};
+};
+
 
 export function createRelayEnvironment(tokenProvider: TokenProvider) {
   return new Environment({
