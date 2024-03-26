da02c45fd0caab9ad121e187a8243951992d6068
mnaegel <mnaegel@uwaterloo.ca>
2024-03-25 21:32:53 -0400

    Relax unique constraint of section to avoid edge case where section from a different campus

---
+++ CourseManager/Data/Migrations/20240324152013_UpdateUniqueConstraint.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class UpdateUniqueConstraint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Sections_Number_CourseOfferingId",
+                table: "Sections");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sections_Number_CourseOfferingId_ClassNumber",
+                table: "Sections",
+                columns: new[] { "Number", "CourseOfferingId", "ClassNumber" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Sections_Number_CourseOfferingId_ClassNumber",
+                table: "Sections");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sections_Number_CourseOfferingId",
+                table: "Sections",
+                columns: new[] { "Number", "CourseOfferingId" },
+                unique: true);
+        }
+    }
+}
