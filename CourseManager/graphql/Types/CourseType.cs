854f12641dcf43a657ebba0484c548b12c7952d5
mnnaegel <mnaegel@uwaterloo.ca>
2024-05-12 13:06:12 -0400

    has prereqs resolver

--- CourseManager/graphql/Types/CourseType.cs
+++ CourseManager/graphql/Types/CourseType.cs
@@ -6,7 +6,7 @@ namespace graphql.Types;
 
 public class CourseType : ObjectType<Course>
 {
-    public class CourseReviewAggregate 
+    public class CourseReviewAggregate
     {
         public double AverageLiked { get; set; }
         public double AverageEasiness { get; set; }
@@ -14,7 +14,7 @@ public class CourseType : ObjectType<Course>
         public int TotalComments { get; set; }
         public int TotalReviews { get; set; }
     }
-    
+
     protected override void Configure(IObjectTypeDescriptor<Course> descriptor)
     {
         descriptor.Field(f => f.Faculty)
@@ -30,18 +30,30 @@ public class CourseType : ObjectType<Course>
                 var courseOfferingGroupedDataLoader = context.Service<CourseOfferingGroupedDataLoader>();
                 return courseOfferingGroupedDataLoader.LoadAsync(context.Parent<Course>().Id, context.RequestAborted);
             });
-        
+
         descriptor.Field("code").Type<NonNullType<StringType>>()
             .Resolve(async context =>
             {
                 var course = context.Parent<Course>();
                 var facultyDataLoader = context.Service<FacultyBatchDataLoader>();
                 var faculty = await facultyDataLoader.LoadAsync(course.FacultyId, context.RequestAborted);
-                
+
                 return faculty.Abbreviation + " " + course.Number;
             });
-        
-        
+
+
         descriptor.Field("rating").ResolveWith<CourseResolver>(r => r.GetRating(default!, default!));
+
+        descriptor.Field("has_prereqs")
+            .Resolve(context =>
+            {
+                var course = context.Parent<Course>();
+                if (string.IsNullOrWhiteSpace(course.Prerequisites))
+                {
+                    return false;
+                }
+
+                return true;
+            });
     }
 }
